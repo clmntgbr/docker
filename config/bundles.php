9d45c4610bfa61e3b144a2b473d5f79440a410b1
clement <clement.goubier@display-interactive.com>
2024-01-27 11:20:15 +0100

    Update to symfony 7.0

--- config/bundles.php
+++ config/bundles.php
@@ -2,7 +2,4 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
